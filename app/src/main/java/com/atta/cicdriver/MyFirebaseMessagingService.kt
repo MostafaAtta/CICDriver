2a9b04202b8948f0c27233d167728bf4ca192723
mostafa.atta <Mos271012>
2021-07-02 15:15:30 +0200

    correct driver account creation

--- app/src/main/java/com/atta/cicdriver/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/atta/cicdriver/MyFirebaseMessagingService.kt
@@ -1,9 +1,17 @@
 package com.atta.cicdriver
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -12,6 +20,64 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         if(remoteMessage.notification != null){
             Log.d("FCM", "FCM message received")
+
+
+            val title = remoteMessage.notification!!.title
+            val body = remoteMessage.notification!!.body
+            showNotification(title, body)
         }
+    }
+
+
+    fun showNotification(title: String?, body: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val notification = NotificationCompat.Builder(this, Companion.CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(R.drawable.cic)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        notificationManager.notify(100, notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channelName = "channelName"
+        val channel = NotificationChannel(Companion.CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH).apply {
+            description = "My channel description"
+            enableLights(true)
+            lightColor = Color.GREEN
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+
+    override fun onNewToken(token: String) {
+        Log.d("FCM", "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        //sendRegistrationToServer(token)
+    }
+
+    private fun getNotificationIcon(): Int {
+        val useWhiteIcon =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+        return if (useWhiteIcon) R.mipmap.ic_launcher else R.mipmap.ic_launcher
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "my_channel"
     }
 }
